1e231618bff1580e3c4db5f3326487aa8a7c90c0
CAPTAIN-KENWAY <42762837+CAPTAIN-KENWAY@users.noreply.github.com>
2023-01-06 22:02:43 +0530

    updated main.py - 06/01/2023

--- main.py
+++ main.py
@@ -242,6 +242,6 @@ def delete(post_id):
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+#     with app.app_context():
+#         db.create_all()
     app.run(host='0.0.0.0', port=5000)
